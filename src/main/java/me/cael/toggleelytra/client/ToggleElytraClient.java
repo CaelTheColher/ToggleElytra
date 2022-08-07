194e48182ab2181f0417b9c7d55a3317c9d2972b
Victor Kohler <16297430+CaelTheColher@users.noreply.github.com>
2022-08-06 21:46:17 -0300

    update gradle.properties

--- src/main/java/me/cael/toggleelytra/client/ToggleElytraClient.java
+++ src/main/java/me/cael/toggleelytra/client/ToggleElytraClient.java
@@ -6,7 +6,6 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.entity.event.v1.EntityElytraEvents;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ScreenTexts;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
